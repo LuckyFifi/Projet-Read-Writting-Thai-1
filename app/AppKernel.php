0d8cc9c9d8608452e7ff70bea1bb8731ae9054e5
Philippe Ravel <warmup95@hotmail.fr>
2016-03-24 22:35:51 +0100

    install FosUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Rw\BlogBundle\RwBlogBundle(),
             new Rw\UserBundle\RwUserBundle(),
+			new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
