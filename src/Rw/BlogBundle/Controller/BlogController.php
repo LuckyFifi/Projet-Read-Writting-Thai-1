fb459672a7f1d3c5fe269529bf773a43d9aa9fbe
Philippe Ravel <warmup95@hotmail.fr>
2016-03-31 17:25:13 +0700

    ajout methode Blog:editAction

--- src/Rw/BlogBundle/Controller/BlogController.php
+++ src/Rw/BlogBundle/Controller/BlogController.php
@@ -181,4 +181,48 @@ class BlogController extends Controller
 		'form'    => $form->createView()
 		));
 	}
+	
+	/**
+	 * @Security("has_role('ROLE_ADMIN')")
+     */ 
+	public function editAction($id)
+	{
+		// On récupère le repository
+		$em = $this->getDoctrine()->getManager();
+		$repository = $em->getRepository('RwBlogBundle:Billet');
+		// On récupère l'entité correspondant à l'id $id
+		$billet = $repository->find($id);
+		// $billet est une instance de Rw\BlogBundle\Entity\Billet
+		// Ou null si aucun billet n'a été trouvé avec l'id $id
+		if($billet === null)
+		{
+			throw $this->createNotFoundException('Billet[id='.$id.'] inexistant.');
+		}
+		// On crée le formulaire
+		$form = $this->createForm(new BilletType, $billet);	
+		// On récupère la requête
+		$request = $this->get('request');
+		// On vérifie qu'elle est de type POST
+		if ($request->getMethod() == 'POST') {
+			// On fait le lien Requête <-> Formulaire
+			// À partir de maintenant, la variable $billet contient les valeurs entrées dans le formulaire par le visiteur
+			$form->bind($request);
+			// On vérifie que les valeurs entrées sont correctes
+			// (Nous verrons la validation des objets en détail dans le prochain chapitre)
+			if ($form->isValid()) {
+				// On l'enregistre notre objet $billet dans la base de données
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($billet);
+				$em->flush();
+				// On redirige vers la page de visualisation du billet nouvellement créé
+				return $this->redirect($this->generateUrl('rwblog_view', array('id' => $billet->getId())));
+			}
+		}
+		// À ce stade :
+		// - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+		// - Soit la requête est de type POST, mais le formulaire n'est pas valide, donc on l'affiche de nouveau
+		return $this->render('RwBlogBundle:Blog:edit.html.twig', array(
+		'form' => $form->createView(),
+		));
+	}
 }
