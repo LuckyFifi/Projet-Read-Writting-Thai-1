46f2fb1cea9e068d64acc3a25f0c3d9fd1cd196f
Philippe Ravel <warmup95@hotmail.fr>
2016-04-04 17:40:42 +0700

    ajout methode Blog:deleteCommentAction + divers

--- src/Rw/BlogBundle/Controller/BlogController.php
+++ src/Rw/BlogBundle/Controller/BlogController.php
@@ -93,39 +93,47 @@ class BlogController extends Controller
 	}
 	
 	/**
-	 * @Security("has_role('ROLE_USER')")
-     */ 
-	public function addCommentAction(Billet $billet)
+	 * @Security("has_role('ROLE_ADMIN')")
+     */
+	public function editAction($id)
 	{
-		$user = $this->getUser();
-		$comment = new Comment();
-		if (null === $user) {
-			// Ici, l'utilisateur est anonyme ou l'URL n'est pas derrière un pare-feu
-			throw new AccessDeniedException('Accès limité aux personnes connectées.');
-		} else {
-			$comment->setUser($user);
-		}		
-		$comment->setBillet($billet);
-		$comment->setAuthor($user);
-		// création de la liste des commentaires pour la vue twig
-		$list_comments = $billet->getComments(); //
-		$form = $this->createForm(new CommentType, $comment);
+		// On récupère le repository
+		$em = $this->getDoctrine()->getManager();
+		$repository = $em->getRepository('RwBlogBundle:Billet');
+		// On récupère l'entité correspondant à l'id $id
+		$billet = $repository->find($id);
+		// $billet est une instance de Rw\BlogBundle\Entity\Billet
+		// Ou null si aucun billet n'a été trouvé avec l'id $id
+		if($billet === null)
+		{
+			throw $this->createNotFoundException('Billet[id='.$id.'] inexistant.');
+		}
+		// On crée le formulaire
+		$form = $this->createForm(new BilletType, $billet);	
+		// On récupère la requête
 		$request = $this->get('request');
+		// On vérifie qu'elle est de type POST
 		if ($request->getMethod() == 'POST') {
+			// On fait le lien Requête <-> Formulaire
+			// À partir de maintenant, la variable $billet contient les valeurs entrées dans le formulaire par le visiteur
 			$form->bind($request);
+			// On vérifie que les valeurs entrées sont correctes
 			if ($form->isValid()) {
+				// On l'enregistre notre objet $billet dans la base de données
 				$em = $this->getDoctrine()->getManager();
-				$em->persist($comment);
+				$em->persist($billet);
 				$em->flush();
 				// On définit un message flash
-				$this->get('session')->getFlashBag()->add('info', 'Le commentaire a bien été enregistré !');
+				$this->get('session')->getFlashBag()->add('info', 'Le billet a bien été modifié !');
+				// On redirige vers la page de visualisation du billet nouvellement créé
 				return $this->redirect($this->generateUrl('rwblog_view', array('id' => $billet->getId())));
 			}
 		}
-		return $this->render('RwBlogBundle:Blog:addcomment.html.twig', array(
+		// À ce stade :
+		// - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+		// - Soit la requête est de type POST, mais le formulaire n'est pas valide, donc on l'affiche de nouveau
+		return $this->render('RwBlogBundle:Blog:edit.html.twig', array(
 		'form' => $form->createView(),
-		'billet' => $billet,
-		'comments' => $list_comments 
 		));
 	}
 	
@@ -165,6 +173,43 @@ class BlogController extends Controller
 		return $this->render('RwBlogBundle:Blog:delete.html.twig', array(
 		'billet' => $billet,
 		'form'    => $form->createView()
+		));
+	}
+	
+	/**
+	 * @Security("has_role('ROLE_USER')")
+     */ 
+	public function addCommentAction(Billet $billet)
+	{
+		$user = $this->getUser();
+		$comment = new Comment();
+		if (null === $user) {
+			// Ici, l'utilisateur est anonyme ou l'URL n'est pas derrière un pare-feu
+			throw new AccessDeniedException('Accès limité aux personnes connectées.');
+		} else {
+			$comment->setUser($user);
+		}		
+		$comment->setBillet($billet);
+		$comment->setAuthor($user);
+		// création de la liste des commentaires pour la vue twig
+		$list_comments = $billet->getComments(); 
+		$form = $this->createForm(new CommentType, $comment);
+		$request = $this->get('request');
+		if ($request->getMethod() == 'POST') {
+			$form->bind($request);
+			if ($form->isValid()) {
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($comment);
+				$em->flush();
+				// On définit un message flash
+				$this->get('session')->getFlashBag()->add('info', 'Le commentaire a bien été enregistré !');
+				return $this->redirect($this->generateUrl('rwblog_view', array('id' => $billet->getId())));
+			}
+		}
+		return $this->render('RwBlogBundle:Blog:addcomment.html.twig', array(
+		'form' => $form->createView(),
+		'billet' => $billet,
+		'comments' => $list_comments 
 		));
 	}
 	
@@ -226,46 +271,42 @@ class BlogController extends Controller
 	
 	/**
 	 * @Security("has_role('ROLE_ADMIN')")
-     */
-	public function editAction($id)
+     */ 
+	public function deleteCommentAction($id)
 	{
 		// On récupère le repository
 		$em = $this->getDoctrine()->getManager();
-		$repository = $em->getRepository('RwBlogBundle:Billet');
+		$repository = $em->getRepository('RwBlogBundle:Comment');
 		// On récupère l'entité correspondant à l'id $id
-		$billet = $repository->find($id);
+		$comment = $repository->find($id);
 		// $billet est une instance de Rw\BlogBundle\Entity\Billet
 		// Ou null si aucun billet n'a été trouvé avec l'id $id
-		if($billet === null)
+		if($comment === null)
 		{
-			throw $this->createNotFoundException('Billet[id='.$id.'] inexistant.');
+			throw $this->createNotFoundException('Commentaire[id='.$id.'] inexistant.');
 		}
-		// On crée le formulaire
-		$form = $this->createForm(new BilletType, $billet);	
-		// On récupère la requête
-		$request = $this->get('request');
-		// On vérifie qu'elle est de type POST
+		$billet = $comment->getBillet();
+		// On crée un formulaire vide, qui ne contiendra que le champ CSRF
+		// Cela permet de protéger la suppression d'article contre cette faille
+		$form = $this->createFormBuilder()->getForm();
+		$request = $this->getRequest();
 		if ($request->getMethod() == 'POST') {
-			// On fait le lien Requête <-> Formulaire
-			// À partir de maintenant, la variable $billet contient les valeurs entrées dans le formulaire par le visiteur
 			$form->bind($request);
-			// On vérifie que les valeurs entrées sont correctes
-			if ($form->isValid()) {
-				// On l'enregistre notre objet $billet dans la base de données
-				$em = $this->getDoctrine()->getManager();
-				$em->persist($billet);
-				$em->flush();
+			if ($form->isValid()) { // Ici, isValid ne vérifie donc que le CSRF
+				// suppression du commentaire
+				$em->remove($comment);
+				$em->flush(); // Exécute un DELETE sur $comment
 				// On définit un message flash
-				$this->get('session')->getFlashBag()->add('info', 'Le billet a bien été modifié !');
-				// On redirige vers la page de visualisation du billet nouvellement créé
+				$this->get('session')->getFlashBag()->add('info', 'Le commentaire a bien été supprimé !');
+		        // Puis on redirige vers l'accueil
 				return $this->redirect($this->generateUrl('rwblog_view', array('id' => $billet->getId())));
 			}
 		}
-		// À ce stade :
-		// - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
-		// - Soit la requête est de type POST, mais le formulaire n'est pas valide, donc on l'affiche de nouveau
-		return $this->render('RwBlogBundle:Blog:edit.html.twig', array(
-		'form' => $form->createView(),
+		// Si la requète est en GET, on affiche une page de confirmation avant de supprimer
+		return $this->render('RwBlogBundle:Blog:deletecomment.html.twig', array(
+		'comment' => $comment,
+		'form'    => $form->createView()
 		));
 	}
+	
 }
